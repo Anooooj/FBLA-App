3ff08704a52e93b741064d0c0372d822c5d86034
Anooooj <anooooj@protonmail.com>
2019-11-27 16:53:07 -0500

    merge

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { DataResolverService } from './resolver/data-resolver.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'calendar',
+    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'chapter',
+    loadChildren: () => import('./chapter/chapter.module').then( m => m.ChapterPageModule)
+  },
+  {
+    path: 'eventsignup',
+    loadChildren: () => import('./eventsignup/eventsignup.module').then( m => m.EventsignupPageModule)
+  },
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  },
+  {
+    path: 'details/:id',
+    resolve: {
+      special: DataResolverService
+    },
+    loadChildren: './details/details.module#DetailsPageModule'
+  },
+  {
+    path: 'calendar/:id',
+    resolve: {
+      special: DataResolverService
+    },
+    loadChildren: './calendar/calendar.module#CalendarPageModule'
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  }
+];
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
