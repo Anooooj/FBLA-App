f46633a1dadc85c541f2c40aeb73b7b14117a5e4
Anooooj <anooooj@protonmail.com>
2019-12-01 14:28:07 -0500

    i

--- src/app/join/join.page.ts
+++ src/app/join/join.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { MenuController } from '@ionic/angular';
 import { DataService } from '../services/data.service';
 import { MenuController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
